249d8b33782ea053c42260b1e2a3981e310fe68f
Belal <BelalGit@gmail.com>
2026-06-03 19:27:48 +0300

    Add project files.

---
+++ HMS.PL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HMS.PL.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
